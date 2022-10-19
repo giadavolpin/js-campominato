8157f43a70a4f4fdf9a3a392a34b80024ac18523
giadavolpin <volpingiada@gmail.com>
2022-10-19 17:49:44 +0200

    numer

--- script.js
+++ script.js
@@ -1,56 +1,55 @@
-/* L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-Bonus
-Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe; */
-
+/* Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
+L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+I numeri nella lista delle bombe non possono essere duplicati.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+BONUS:
+1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
+2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste */
 
 
 //richiamo il bottone dall'HTML
-const playButton = document.getElementById('playButton');
 
+
+const playButton = document.getElementById('playButton');
+let numeroCell = 100;
 function play (){
     console.log('inizio gioco...')
-    const numeroCell = 100;
     
-    function Celle(numeroCell){
-        const cell = document.createElement('div');
-        cell.className = 'square';
-        cell.innerHTML = `
-        <span>${numeroCell}<span>
-        `;
-        return cell;
-    }
-
-    // al click del bottone si genererà la griglia di gioco. 
-    function campoMinato(){
-        const campoGioco = document.getElementById('campoDiGioco'); 
-        const grid = document.createElement('div');
-        grid.className = 'grid' //griglia
-        for (let i= 1; i<= 100; i++){
-            const cella =  numeroCell(i);
-            grid.appendChild(cell);
-        }
-        campoGioco.appendChild(grid);
-    }
-   
+    
+    Celle(numeroCell);
+    campoMinato();
 }
- 
-//chiamo la funzione fuori dalla funzione stessa
-play();
-
 // L'utente clicca su un bottone 
-playButton.addEventListener('click', playButton);
+playButton.addEventListener('click', play);
 
-
+function Celle(numeroCell){
+    const cell = document.createElement('div');
+    cell.className = 'square';
+    cell.innerHTML = `
+    <span>${numeroCell}<span>
+    `;
+    return cell;
+}
+// al click del bottone si genererà la griglia di gioco. 
+function campoMinato(){
    
-
-
-
-
+    const campoGioco = document.getElementById('campoDiGioco'); 
+    const grid = document.createElement('div');
+    grid.className = 'grid' //griglia
+    for (let i= 1; i<= numeroCell; i++){
+        cella =  i;
+        grid.appendChild(cella);
+    }
+    campoGioco.appendChild(grid);
+    
+}
+//chiamo la funzione iniziale
+play();
 
 // Ogni cella ha un numero progressivo, da 1 a 100.
